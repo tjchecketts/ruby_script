bd899cdb1686ef2e46ea2eab512823046f5e53e2
“TJ <tylerchecketts@gmail.com>
2017-11-07 14:27:11 -0700

    add while loop

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,10 +1,26 @@
 puts "what's your favorite number?"
 your_num = []
-your_num << gets.strip
+your_num << gets.strip.to_i
 
-puts "your number is #{your_num}"
+puts "your favorite number is #{your_num}"
 
-puts "what's another favorite number?"
-your_num << gets.strip
+while true
+  puts"""
+  Would you like to add another favorite number?
+  type 1 for yes and type 2 for no
+  """
 
-puts "your numbers are #{your_num}"
+  num_choice = gets.strip.to_i
+  case num_choice
+
+  when 1
+  puts "what's another favorite number?"
+  your_num << gets.strip.to_i
+
+  puts "your favorite numbers are #{your_num}"
+
+  when 2
+    puts "thanks for playing"
+    exit
+  end
+end
