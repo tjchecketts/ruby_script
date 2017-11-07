1dc783c04cf0697226059e669d03f12ce7afd133
“TJ <tylerchecketts@gmail.com>
2017-11-07 13:57:42 -0700

    first commit

---
+++ ruby_script.rb
@@ -0,0 +1,10 @@
+puts "what's your favorite number?"
+your_num = []
+your_num << gets.strip
+
+puts "your number is #{your_num}"
+
+puts "what's another favorite number?"
+your_num << gets.strip
+
+puts "your numbers are #{your_num}"
